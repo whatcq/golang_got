fd675e50ef7dd3a34730bd2548a6f5ecbd92efca
cqiu <gdaymate@126.com>
2018-07-07 11:35:15 +0800

    a uncompleted morning...接人/送人没想清楚

--- lift.go
+++ lift.go
@@ -1,42 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main(){
+func main() {
 	var floors [34]int
-	l:=&lift{
-		floor: 0,
+	l := &lift{
+		floor:     0,
 		direction: 0,
-		weight:0,
-		floors:floors,//数组语法？
+		weight:    0,
+		floors:    floors, //数组语法？
 	}
 	go l.listen()
 }
 
 type lift struct {
-	floor int
+	floor     int
 	direction int
-	weight int
-	floors [34]int
+	weight    int
+	floors    [34]int
 }
+
 //direction: 0 电梯内部请求到哪一层；1/-1 哪一层要上/下
-func (l *lift)listen(){
+func (l *lift) listen() {
 	var floor int
 	var direction bool
-	for{
+	for {
 		fmt.Println("floor & direction: ")
+		// 等待输入？
 	}
 }
-func (l *lift)move(floor int, direction bool){
-	for{//这个函数的职责、逻辑不清楚
-		for i:=l.floor;i< ;  {
-			
-		}
-		for floor, direction = range l.floors{
-			if 
+// 扫描请求，并move
+func (l *lift) run() {
+	// 死循环该怎样写?
+	// l.floors会改变吗？需要channel吗？
+	for { //这个函数的职责、逻辑不清楚
+		for floor, direction := range l.floors {
+			if direction != 0 && floor > l.floor*l.direction {
+				//move 1 floor with 1 second
+				l.floor += l.direction
+				time.Sleep(1 * time.Second)
+				fmt.Println("-> ", l.floor)
+				break
+			}
 		}
 	}
-}
-func (l *lift)stop(floor int){
-
 }
